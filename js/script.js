c11e03ef2f91bbeeff116ef57b9c3ebdbdcfe36e
maureenndiema <ndiemam@gmail.com>
2022-03-19 16:08:43 +0300

    add image

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function (){
+    var price , crust_price, topping_price ;
+let total = 0;
+function Getpizza( name,size,crust,topping, total ){
+  this.name = name;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.total = total;
 
 });
-function () {
-
-}
