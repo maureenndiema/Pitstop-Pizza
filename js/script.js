713acc5ecdaa729f78d72f5de138f88ce50b0ebf
maureenndiema <ndiemam@gmail.com>
2022-03-19 19:42:10 +0300

    commit all changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,46 @@
-$(document).ready(function (){
-    var price , crust_price, topping_price ;
-let total = 0;
-function Getpizza( name,size,crust,topping, total ){
+//Business Logic
+function Order(name, size, crust, topping, total) {
   this.name = name;
   this.size = size;
   this.crust = crust;
   this.topping = topping;
   this.total = total;
+}
+Order.prototype.newOrder 
 
+//user Interface Logic
+$(document).ready(function(){
+  $("form#new-order").submit(function (event){
+    event.preventDefault();
+
+    var inputtedName = $("select#name").val();
+    var inputtedSize = $("select#size").val();
+    var inputtedCrust = $("select#crust").val();
+    var inputtedTopping = $("select#topping").val();
+    var inputtedTotal = $("select#total").val();
+
+    var newOrder = new Order(
+      inputtedName, inputtedSize, inputtedCrust, inputtedTopping, inputtedTotal
+    );
+
+    console.log(new0rder);
+    $("tbody#ordermade").append("<tr>"+
+"<td>"+newOrder.name+"</td>"+
+"<td>"+newOrder.size+"</td>"+
+"<td>"+newOrder.crust+"</td>"+
+"<td>"+newOrder.topping+"</td>"+
+"<td>"+newOrder.total+"</td>"+
++"</tr>")
+
+   
 });
+});
+
+
+// append("<tr>"+
+// "<td>"+newOrder.name+"</td>"+
+// "<td>"+newOrder.size+"</td>"+
+// "<td>"+newOrder.crust+"</td>"+
+// "<td>"+newOrder.topping+"</td>"+
+// "<td>"+newOrder.total+"</td>"+
+// +"</tr>")
